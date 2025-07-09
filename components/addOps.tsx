c9c741b74d15a342ad4fb39c263a62528dccf865
Codebox124 <rajcodes733@gmail.com>
2025-07-09 17:19:13 +0100

    updated

--- components/addOps.tsx
+++ components/addOps.tsx
@@ -48,7 +48,7 @@ export const AddOpsModal: React.FC<AddOpsModalProps> = ({ opened, onClose, onSub
                     borderRadius: '8px',
                 },
                 title: { color: 'white', fontWeight: 600 },
-                header: { borderBottom: 'none' },
+                header: { borderBottom: 'none',backgroundColor: '#0f172a', },
                 body: { color: 'white' },
                 close: {
                     color: 'white',
